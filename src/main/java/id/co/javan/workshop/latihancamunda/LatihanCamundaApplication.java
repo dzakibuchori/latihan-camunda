cd3f215c3d37f76c720ab0cea50ae91c38bdf62f
Dzaki Buchori <dzakibuchori@gmail.com>
2020-12-04 13:38:08 +0700

    initiate files

---
+++ src/main/java/id/co/javan/workshop/latihancamunda/LatihanCamundaApplication.java
@@ -0,0 +1,13 @@
+package id.co.javan.workshop.latihancamunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanCamundaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatihanCamundaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/co/javan/workshop/latihancamunda/LatihanCamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.javan.workshop.latihancamunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanCamundaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
