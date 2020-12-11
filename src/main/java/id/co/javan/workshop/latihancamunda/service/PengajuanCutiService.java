5f376618012fef541bc32f7e77fc13805a024871
Dzaki Buchori <dzakibuchori@gmail.com>
2020-12-11 14:15:36 +0700

    add listApprovalAtasan()

--- src/main/java/id/co/javan/workshop/latihancamunda/service/PengajuanCutiService.java
+++ src/main/java/id/co/javan/workshop/latihancamunda/service/PengajuanCutiService.java
@@ -1,9 +1,13 @@
 package id.co.javan.workshop.latihancamunda.service;
 
 import id.co.javan.workshop.latihancamunda.model.PengajuanCuti;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Date;
 
 public interface PengajuanCutiService {
     PengajuanCuti start(String nama, Date tanggal);
+
+    Page<PengajuanCuti> listApproveAtasan(Pageable pageable);
 }
--- src/main/java/id/co/javan/workshop/latihancamunda/service/impl/PengajuanCutiServiceImpl.java
+++ src/main/java/id/co/javan/workshop/latihancamunda/service/impl/PengajuanCutiServiceImpl.java
@@ -5,17 +5,24 @@ import id.co.javan.workshop.latihancamunda.repository.PengajuanCutiRepository;
 import id.co.javan.workshop.latihancamunda.service.CamundaProcessService;
 import id.co.javan.workshop.latihancamunda.service.PengajuanCutiService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class PengajuanCutiServiceImpl implements PengajuanCutiService {
     private PengajuanCutiRepository pengajuanCutiRepository;
     private CamundaProcessService camundaProcessService;
+
+    private static final String PROCESS_DEF_KEY = "pengajuan_cuti";
 
     @Autowired
     public PengajuanCutiServiceImpl(PengajuanCutiRepository pengajuanCutiRepository, CamundaProcessService camundaProcessService) {
@@ -32,9 +39,16 @@ public class PengajuanCutiServiceImpl implements PengajuanCutiService {
 
         Map<String, Object> variables = new HashMap<>();
         variables.put("tanggal", tanggal);
-        ProcessInstance pe = camundaProcessService.startProcess("pengajuan_cuti", String.valueOf(cuti.getId()), variables);
+        ProcessInstance pe = camundaProcessService.startProcess(PROCESS_DEF_KEY, String.valueOf(cuti.getId()), variables);
         cuti.setProcessInstanceId(pe.getProcessInstanceId());
 
         return pengajuanCutiRepository.save(cuti);
+    }
+
+    @Override
+    public Page<PengajuanCuti> listApproveAtasan(Pageable pageable) {
+        List<Task> tasks = camundaProcessService.getActiveTasks(PROCESS_DEF_KEY, "approval_atasan");
+        List<String> processInstanceIds = tasks.stream().map(Task::getProcessInstanceId).collect(Collectors.toList());
+        return pengajuanCutiRepository.findAllByProcessInstanceId(processInstanceIds, pageable);
     }
 }
