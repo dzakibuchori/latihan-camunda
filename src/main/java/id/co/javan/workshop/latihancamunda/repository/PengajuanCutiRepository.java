6017d8734ffdc15b8fc23ffd58647b92ca10143e
Dzaki Buchori <dzakibuchori@gmail.com>
2020-12-11 13:38:11 +0700

    add PengajuanCutiRepository

---
+++ src/main/java/id/co/javan/workshop/latihancamunda/repository/PengajuanCutiRepository.java
@@ -0,0 +1,7 @@
+package id.co.javan.workshop.latihancamunda.repository;
+
+import id.co.javan.workshop.latihancamunda.model.PengajuanCuti;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PengajuanCutiRepository extends JpaRepository<PengajuanCuti, Long> {
+}
