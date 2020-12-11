ac2ec433d77b6e934cf7e3d55c559046b8c99d02
Dzaki Buchori <dzakibuchori@gmail.com>
2020-12-11 13:38:36 +0700

    add PengajuanCutiService and its impl

---
+++ src/main/java/id/co/javan/workshop/latihancamunda/service/PengajuanCutiService.java
@@ -0,0 +1,9 @@
+package id.co.javan.workshop.latihancamunda.service;
+
+import id.co.javan.workshop.latihancamunda.model.PengajuanCuti;
+
+import java.util.Date;
+
+public interface PengajuanCutiService {
+    PengajuanCuti start(String nama, Date tanggal);
+}
---
+++ src/main/java/id/co/javan/workshop/latihancamunda/service/impl/PengajuanCutiServiceImpl.java
@@ -0,0 +1,40 @@
+package id.co.javan.workshop.latihancamunda.service.impl;
+
+import id.co.javan.workshop.latihancamunda.model.PengajuanCuti;
+import id.co.javan.workshop.latihancamunda.repository.PengajuanCutiRepository;
+import id.co.javan.workshop.latihancamunda.service.CamundaProcessService;
+import id.co.javan.workshop.latihancamunda.service.PengajuanCutiService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class PengajuanCutiServiceImpl implements PengajuanCutiService {
+    private PengajuanCutiRepository pengajuanCutiRepository;
+    private CamundaProcessService camundaProcessService;
+
+    @Autowired
+    public PengajuanCutiServiceImpl(PengajuanCutiRepository pengajuanCutiRepository, CamundaProcessService camundaProcessService) {
+        this.pengajuanCutiRepository = pengajuanCutiRepository;
+        this.camundaProcessService = camundaProcessService;
+    }
+
+    @Override
+    public PengajuanCuti start(String nama, Date tanggal) {
+        PengajuanCuti cuti = new PengajuanCuti();
+        cuti.setNama(nama);
+        cuti.setTanggal(tanggal);
+        cuti = pengajuanCutiRepository.save(cuti);
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("tanggal", tanggal);
+        ProcessInstance pe = camundaProcessService.startProcess("pengajuan_cuti", nama, variables);
+        cuti.setProcessInstanceId(pe.getProcessInstanceId());
+
+        return pengajuanCutiRepository.save(cuti);
+    }
+}
